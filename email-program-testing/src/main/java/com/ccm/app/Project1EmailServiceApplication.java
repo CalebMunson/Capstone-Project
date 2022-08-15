68c7e863a961a86efd7cf0ac4381072b6ace6c70
CalebMunson <caleb.munson44@gmail.com>
2022-08-15 10:01:42 -0500

    Files for an attempt of writing the email service using online examples as an aid

---
+++ email-program-testing/src/main/java/com/ccm/app/Project1EmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ccm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1EmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1EmailServiceApplication.class, args);
+	}
+
+}
---
+++ email-program-testing/src/test/java/com/ccm/app/Project1EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccm.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
